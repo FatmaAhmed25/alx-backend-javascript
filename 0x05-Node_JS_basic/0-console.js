d7c12aaa27cf14011b34f9803c1c85c4d0350053
FatmaAhmed25 <fatmaahmed6555@gmail.com>
2024-11-29 11:19:32 +0200

    0x05

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Fatma Ahmed<https://github.com/FatmaAhmed25>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
